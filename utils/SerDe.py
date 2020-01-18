754da8cba2cd7d5732f1d2ad910eefbee0859aad
Madhav Khosla <madhav.khoslaa@gmail.com>
2020-01-18 17:28:49 +0530

    Serde working

--- utils/SerDe.py
+++ utils/SerDe.py
@@ -1,4 +1,7 @@
 import pickle
+import base64
+import pickle 
+import os
 
 
 class SerDe():
@@ -6,11 +9,27 @@ class SerDe():
         pass
 
     def Serialise(self, object):
-        picklefile = open('dumpdata', 'rb')
+        """Returns the binary data into a base 64 encoded values"""
+        payload = {}
+        picklefile = open('datadump', 'wb')
         pickle.dump(object, picklefile)
+        picklefile.close()
+        f=open("datadump", "rb")
+        payload['content'] = base64.b64encode(f.read())
+        os.system("rm -rf datadump")
+        return payload
 
-
-
-
-    def Deserialise(self, bits):
-        pass
+    def Deserialise(self, payload):
+        """Expects a dictionary of base54 encoded binary values of a 
+        Serialised object"""
+        #Decoding the base64 values
+        fileData = base64.b64decode(payload['content'])
+        #Creating a byte array to write the file
+        newFileByteArray = bytearray(fileData)
+        f= open("datadump","w+b")
+        f.write(newFileByteArray)
+        picklefile = open("datadump", "rb")
+        object = pickle.load(picklefile, encoding="bytes")
+        picklefile.close()
+        os.system("rm -rf datadump")
+        return object
